edbf60eedfdf29ff507ad3426e81ae309f996f64
zong4 <zzoonng@163.com>
2025-09-15 18:34:43 +0200

    change subsystem for window, but window paused when resize window

--- Editor/xmake.lua
+++ Editor/xmake.lua
@@ -1,6 +1,10 @@
 target("Editor")
     set_kind("binary")
 
+    if is_plat("windows") then
+        add_ldflags("/subsystem:console")
+    end
+
     add_deps("Core")
     add_deps("Function")
     add_deps("Engine")
