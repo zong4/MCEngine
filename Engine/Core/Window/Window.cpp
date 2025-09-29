3a14b76f5dc17501e9002b1e4c955d6cccbef1f1
zong4 <zzoonng@163.com>
2025-09-29 22:25:04 +0200

    change opengl version

--- Engine/Core/Window/Window.cpp
+++ Engine/Core/Window/Window.cpp
@@ -62,10 +62,12 @@ void MCEngine::Window::Init()
     ENGINE_PROFILE_FUNCTION();
 
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #ifdef __APPLE__
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, 1);
 #endif
     LOG_ENGINE_INFO("GLFW version: " + std::string(glfwGetVersionString()));
