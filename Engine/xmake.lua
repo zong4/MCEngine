9dd3108e55f999cb0aebe2e30b929e78b4cf66d0
zong4 <zzoonng@163.com>
2025-09-29 20:18:47 +0200

    add update structure

--- Engine/xmake.lua
+++ Engine/xmake.lua
@@ -1,7 +1,42 @@
---set_runtimes("MTd")
+-- Common
+add_requires("spdlog", "glm")
 
-includes("Core")
-includes("Function")
+-- Graphics
+add_requires("glfw", "glad", "stb")
+add_requires("imgui v1.92.1-docking", {configs = {glfw_opengl3 = true}})
+
+-- ECS
+add_requires("entt")
+
+target("Core")
+    set_kind("static")
+    
+    add_headerfiles("Core/**/*.hpp")
+    add_files("Core/**/*.cpp")
+    add_includedirs("Core", {public = true})
+
+    -- Common
+    add_packages("spdlog")
+    add_packages("glm", {public = true})
+
+    -- Graphics
+    add_packages("glfw", "glad", "stb");
+    add_packages("imgui", {public = true});
+
+target("Function")
+    set_kind("static")
+
+    add_deps("Core")
+
+    add_headerfiles("Function/**/*.hpp")
+    add_files("Function/**/*.cpp")
+    add_includedirs("Function", {public = true})
+
+    -- ECS
+    add_packages("entt", {public = true})
+
+
+--set_runtimes("MTd")
 
 target("Engine")
     set_kind("static")
