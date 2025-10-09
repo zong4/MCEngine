bc1432104e62d50cf6174e245f7e08c3ebf5d29e
zong4 <zzoonng@163.com>
2025-10-09 10:58:39 +0200

    fix example scene

--- Editor/Sources/Scene/ExampleScene.cpp
+++ Editor/Sources/Scene/ExampleScene.cpp
@@ -1,6 +1,6 @@
 #include "ExampleScene.hpp"
 
-MCEditor::ExampleScene::ExampleScene()
+MCEditor::ExampleScene::ExampleScene() : MCEngine::Scene("ExampleScene")
 {
     m_MainCamera = AddCamera("MainCamera",
                              MCEngine::TransformComponent(glm::vec3(0.0f, 5.0f, 8.0f), glm::vec3(-30.0f, 0.0f, 0.0f)),
